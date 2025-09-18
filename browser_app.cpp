c0cbe7c83b6928b126d92a11963b06ace5463cc1
Eslam Linux <linuxeslam@gmail.com>
2025-09-18 22:45:25 +0300

    Create browser_app.cpp

---
+++ browser_app.cpp
@@ -0,0 +1,7 @@
+#include "browser_app.h"
+#include <wx/webview.h>
+#include "settings_manager.h"
+#include <wx/log.h>
+#include <wx/intl.h>
+#include <wx/translation.h>
+#include "browser_constants.h"

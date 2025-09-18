0ce81265a910eb0147254b64f6a997f54e415839
Eslam Linux <linuxeslam@gmail.com>
2025-09-18 23:06:22 +0300

    Create settings_manager.h

---
+++ settings_manager.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <wx/wx.h>
+#include <map>
+#include <string>
+#include <vector>
+#include "i_settings_observer.h"
+
+/**
+ * مدير الإعدادات (نمط Singleton)
+ * يتعامل مع تخزين واسترجاع إعدادات المتصفح
+ */

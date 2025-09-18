516551ffcec1c6b1f01316108f7f48ec1448402c
Eslam Linux <linuxeslam@gmail.com>
2025-09-18 23:09:48 +0300

    Create browser_tab.h

---
+++ browser_tab.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <wx/wx.h>
+#include <wx/webview.h>
+#include "bookmark_manager.h"
+#include "i_browser_tab_observer.h"
+#include "settings_manager.h"
+
+/**
+ * علامة تبويب المتصفح
+ * تمثل علامة تبويب واحدة في المتصفح
+ */

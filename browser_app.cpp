8ade59437b549f199c55b53e5d37dacefc148979
Eslam Linux <linuxeslam@gmail.com>
2025-09-18 22:46:06 +0300

    Update browser_app.cpp

--- browser_app.cpp
+++ browser_app.cpp
@@ -25,4 +25,39 @@ bool BrowserApp::OnInit() {
     return true;
 }
 
+void BrowserApp::InitLocale() {
+    // تهيئة دعم اللغة العربية
+    wxLocale* locale = new wxLocale(wxLANGUAGE_ARABIC);
+    
+    // التأكد من أن wxWidgets يستخدم ترميز UTF-8
+    wxConvCurrent = &wxConvUTF8;
+    
+    // إضافة مسار الترجمة إذا كان لديك ملفات ترجمة
+    locale->AddCatalogLookupPathPrefix("./locale");
+    
+    // تعيين اللغة الافتراضية
+    locale->AddCatalog("browser");
+    
+    // حفظ الإشارة إلى الكائن locale
+    m_locale.reset(locale);
+}
 
+bool BrowserApp::CheckWebViewBackend() {
+    // التحقق من توفر محرك عرض الويب
+    if (!wxWebView::IsBackendAvailable(wxWebViewBackendDefault)) {
+        wxMessageBox("لم يتم العثور على محرك عرض ويب مناسب", "خطأ", wxOK | wxICON_ERROR);
+        return false;
+    }
+    
+    return true;
+}
+
+void BrowserApp::InitSettings() {
+    // تهيئة مدير الإعدادات
+    SettingsManager::GetInstance().LoadSettings();
+    
+    // تهيئة سجل الأخطاء
+    wxLog::SetActiveTarget(new wxLogStderr());
+}
+
+wxIMPLEMENT_APP(BrowserApp);

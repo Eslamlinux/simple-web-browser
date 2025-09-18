cc143c7c26e16d1e5c9c2ee9c991d6b4a1d7314b
Eslam Linux <linuxeslam@gmail.com>
2025-09-18 22:51:59 +0300

    Create browser_event.cpp

---
+++ browser_event.cpp
@@ -0,0 +1,6 @@
+#include "browser_event.h"
+
+// تعريف الأحداث المخصصة
+wxDEFINE_EVENT(wxEVT_BROWSER_TITLE_CHANGED, wxCommandEvent);
+wxDEFINE_EVENT(wxEVT_BROWSER_URL_CHANGED, wxCommandEvent);
+wxDEFINE_EVENT(wxEVT_BROWSER_BOOKMARK_ADDED, wxCommandEvent);

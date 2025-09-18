5376822bd3919669cc78214fba7ce6e4f57250ca
Eslam Linux <linuxeslam@gmail.com>
2025-09-18 23:10:30 +0300

    Create browser_frame.h

---
+++ browser_frame.h
@@ -0,0 +1,11 @@
+include <wx/wx.h>
+#include <wx/notebook.h>
+#include <wx/aui/aui.h>
+#include <vector>
+#include <memory>
+#include "browser_tab.h"
+#include "bookmark_manager.h"
+#include "i_browser_tab_observer.h"
+#include "i_bookmark_observer.h"
+#include "i_settings_observer.h"
+#include "browser_constants.h"

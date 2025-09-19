82bdef91611d95b9254009e1242e20653fb845ba
Eslam Linux <linuxeslam@gmail.com>
2025-09-19 06:25:45 +0300

    Add files via upload

---
+++ build/CMakeFiles/browser.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/eslam/88C20D18C20D0C5E/my projects/browser/0.1/bookmark_manager.cpp" "CMakeFiles/browser.dir/bookmark_manager.cpp.o" "gcc" "CMakeFiles/browser.dir/bookmark_manager.cpp.o.d"
+  "/media/eslam/88C20D18C20D0C5E/my projects/browser/0.1/browser_app.cpp" "CMakeFiles/browser.dir/browser_app.cpp.o" "gcc" "CMakeFiles/browser.dir/browser_app.cpp.o.d"
+  "/media/eslam/88C20D18C20D0C5E/my projects/browser/0.1/browser_event.cpp" "CMakeFiles/browser.dir/browser_event.cpp.o" "gcc" "CMakeFiles/browser.dir/browser_event.cpp.o.d"
+  "/media/eslam/88C20D18C20D0C5E/my projects/browser/0.1/browser_frame.cpp" "CMakeFiles/browser.dir/browser_frame.cpp.o" "gcc" "CMakeFiles/browser.dir/browser_frame.cpp.o.d"
+  "/media/eslam/88C20D18C20D0C5E/my projects/browser/0.1/browser_tab.cpp" "CMakeFiles/browser.dir/browser_tab.cpp.o" "gcc" "CMakeFiles/browser.dir/browser_tab.cpp.o.d"
+  "/media/eslam/88C20D18C20D0C5E/my projects/browser/0.1/session_manager.cpp" "CMakeFiles/browser.dir/session_manager.cpp.o" "gcc" "CMakeFiles/browser.dir/session_manager.cpp.o.d"
+  "/media/eslam/88C20D18C20D0C5E/my projects/browser/0.1/settings_manager.cpp" "CMakeFiles/browser.dir/settings_manager.cpp.o" "gcc" "CMakeFiles/browser.dir/settings_manager.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/browser.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/browser.dir/bookmark_manager.cpp.o"
+  "CMakeFiles/browser.dir/bookmark_manager.cpp.o.d"
+  "CMakeFiles/browser.dir/browser_app.cpp.o"
+  "CMakeFiles/browser.dir/browser_app.cpp.o.d"
+  "CMakeFiles/browser.dir/browser_event.cpp.o"
+  "CMakeFiles/browser.dir/browser_event.cpp.o.d"
+  "CMakeFiles/browser.dir/browser_frame.cpp.o"
+  "CMakeFiles/browser.dir/browser_frame.cpp.o.d"
+  "CMakeFiles/browser.dir/browser_tab.cpp.o"
+  "CMakeFiles/browser.dir/browser_tab.cpp.o.d"
+  "CMakeFiles/browser.dir/session_manager.cpp.o"
+  "CMakeFiles/browser.dir/session_manager.cpp.o.d"
+  "CMakeFiles/browser.dir/settings_manager.cpp.o"
+  "CMakeFiles/browser.dir/settings_manager.cpp.o.d"
+  "browser"
+  "browser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/browser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

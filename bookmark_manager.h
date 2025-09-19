709782e2672272d10c10d8a0d6633c26b549ae0b
Eslam Linux <linuxeslam@gmail.com>
2025-09-19 06:00:44 +0300

    Update bookmark_manager.h

--- bookmark_manager.h
+++ bookmark_manager.h
@@ -1,3 +1,4 @@
+#pragma once
 
 #include <wx/wx.h>
 #include <wx/listctrl.h>
@@ -5,3 +6,30 @@
 #include <memory>
 #include "bookmark.h"
 #include "i_bookmark_observer.h"
+
+/**
+ * مدير الإشارات المرجعية
+ * يتعامل مع إضافة وإزالة وعرض الإشارات المرجعية
+ */
+class BookmarkManager {
+public:
+    /**
+     * المنشئ
+     */
+    BookmarkManager();
+    
+    /**
+     * الهادم
+     */
+    ~BookmarkManager();
+    
+    /**
+     * إضافة إشارة مرجعية جديدة
+     * @param title عنوان الإشارة المرجعية
+     * @param url رابط الإشارة المرجعية
+     * @return true إذا تمت الإضافة بنجاح، false إذا كانت الإشارة المرجعية موجودة بالفعل
+     */
+    bool AddBookmark(const wxString& title, const wxString& url);
+    
+
+};

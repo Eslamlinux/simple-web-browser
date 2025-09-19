ad25e95670090e693a57cac1145dbbcf564674e5
Eslam Linux <linuxeslam@gmail.com>
2025-09-19 05:59:54 +0300

    Update bookmark.h

--- bookmark.h
+++ bookmark.h
@@ -1,3 +1,48 @@
+#pragma once
 
 #include <wx/wx.h>
 #include <wx/datetime.h>
+
+/**
+ * فئة الإشارة المرجعية
+ * تمثل إشارة مرجعية واحدة في المتصفح
+ */
+class Bookmark {
+public:
+    /**
+     * المنشئ
+     * @param title عنوان الإشارة المرجعية
+     * @param url رابط الإشارة المرجعية
+     */
+    Bookmark(const wxString& title, const wxString& url)
+        : m_title(title), m_url(url), m_dateAdded(wxDateTime::Now()) {}
+    
+    /**
+     * الحصول على عنوان الإشارة المرجعية
+     * @return عنوان الإشارة المرجعية
+     */
+    const wxString& GetTitle() const { return m_title; }
+    
+    /**
+     * الحصول على رابط الإشارة المرجعية
+     * @return رابط الإشارة المرجعية
+     */
+    const wxString& GetUrl() const { return m_url; }
+    
+    /**
+     * الحصول على تاريخ إضافة الإشارة المرجعية
+     * @return تاريخ الإضافة
+     */
+    const wxDateTime& GetDateAdded() const { return m_dateAdded; }
+    
+    /**
+     * تعيين تاريخ إضافة الإشارة المرجعية
+     * @param date التاريخ الجديد
+     */
+    void SetDateAdded(const wxDateTime& date) { m_dateAdded = date; }
+
+private:
+    wxString m_title;       ///< عنوان الإشارة المرجعية
+    wxString m_url;         ///< رابط الإشارة المرجعية
+    wxDateTime m_dateAdded; ///< تاريخ إضافة الإشارة المرجعية
+};
